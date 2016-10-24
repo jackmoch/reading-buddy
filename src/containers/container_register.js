d1c566a3e010267bdbffcf5b48ab03036662dce3
Jack Mocherman <mochermanjack@gmail.com>
2016-10-24 15:34:27 -0500

    bootstrap layout for registration form

--- src/containers/container_register.js
+++ src/containers/container_register.js
@@ -22,16 +22,54 @@ export default class Register extends Component {
 
 	render() {
 		return(
-			<form
-				onSubmit={this.onFormSubmit}>
-				<input />
-				<input />
-				<input />
-				<input />
-				<span>
-					<button>Submit</button>
-				</span>
-			</form>
+			<div className="row centered-form">
+				<div className="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+					<div className="panel panel-default">
+						<div className="panel-heading">
+							<h3 className="panel-title">Sign up for Reading Buddy</h3>
+						</div>
+						<div className="panel-body">
+			    		<form 
+			    			role="form" 
+			    			onSubmit={this.onFormSubmit}
+			    			>
+			    			<div className="row">
+			    				<div className="col-xs-6 col-sm-6 col-md-6">
+			    					<div className="form-group">
+			                <input type="text" name="first_name" id="first_name" className="form-control input-sm" placeholder="First Name" />
+			    					</div>
+			    				</div>
+			    				<div className="col-xs-6 col-sm-6 col-md-6">
+			    					<div className="form-group">
+			    						<input type="text" name="last_name" id="last_name" className="form-control input-sm" placeholder="Last Name" />
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div className="form-group">
+			    				<input type="email" name="email" id="email" className="form-control input-sm" placeholder="Email Address" />
+			    			</div>
+
+			    			<div className="row">
+			    				<div className="col-xs-6 col-sm-6 col-md-6">
+			    					<div className="form-group">
+			    						<input type="password" name="password" id="password" className="form-control input-sm" placeholder="Password" />
+			    					</div>
+			    				</div>
+			    				<div className="col-xs-6 col-sm-6 col-md-6">
+			    					<div className="form-group">
+			    						<input type="password" name="password_confirmation" id="password_confirmation" className="form-control input-sm" placeholder="Confirm Password" />
+			    					</div>
+			    				</div>
+			    			</div>
+			    			
+			    			<input type="submit" value="Register" className="btn btn-info btn-block" />
+			    		
+			    		</form>
+			    	</div>
+					</div>
+				</div>
+			</div>
 		)
 	}
 
