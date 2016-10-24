93756b128b45c338f13620fe7d810ce4c6f21124
Jack Mocherman <mochermanjack@gmail.com>
2016-10-24 11:35:40 -0500

    built basic server to server up client folder

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('port', port)
+
+app.use(express.static('client'))
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
