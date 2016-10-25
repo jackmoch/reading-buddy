618e9b6152771cbd1b07e8e5cacd91e7a55165e4
Jack Mocherman <mochermanjack@gmail.com>
2016-10-25 15:17:14 -0500

    broke register post route out of routes index.js and placed into controllers directory

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,33 +1,10 @@
 'use strict'
 
 const { Router } = require('express')
-const bcrypt = require('bcrypt')
 const router = Router()
-const User = require('../models/user')
 
-router.post('/register', ({ session, body: { username, password, first_name, last_name} }, res, err) => {
-	User.findOne({ username })
-		.then(user => {
-			if (user) {
-				res.json({ msg: 'User is already registered' })
-			} else {
-				return new Promise((resolve, reject) => {
-					bcrypt.hash(password, 15, (err, hash) => {
-						if (err) {
-							reject(err)
-						} else {
-							resolve(hash)
-						}
-					})
-				})
-			}
-		})
-		.then(hash => User.create({ username, password: hash, first_name, last_name }))
-		.then((user) => {
-			session.userId = user.id
-			res.json(user.id)
-		})
-		.catch(err)
-})
+const session = require('../controllers/session')
+
+router.post('/register', session.register)
 
 module.exports = router
