6c80e5b60cfae1e1c4c29dde164a39865f778e00
Jack Mocherman <mochermanjack@gmail.com>
2016-10-25 15:00:01 -0500

    built login reducer

---
+++ src/reducers/reducer_login.js
@@ -0,0 +1,9 @@
+import { LOGIN } from '../actions/index'
+
+export default function(state = '', action) {
+	switch (action.type) {
+		case LOGIN:
+			return action.payload.data
+	}
+	return state
+}
