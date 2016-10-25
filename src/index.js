0c1d6791de3ca9a153eb8898690c775077693110
Jack Mocherman <mochermanjack@gmail.com>
2016-10-25 14:56:57 -0500

    refactored routes to be pulled totally out of routes.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { Router, browserHistory } from 'react-router'
+import { browserHistory } from 'react-router'
 import createLogger from 'redux-logger'
 import ReduxPromise from 'redux-promise'
 
 import reducers from './reducers'
-import routes from './routes'
+import Routes from './routes'
 
 const logger = createLogger()
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore)
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-		<Router history={browserHistory} routes={routes} />
+		<Routes />
 	</Provider >,
 	document.getElementById('entry')
 )
