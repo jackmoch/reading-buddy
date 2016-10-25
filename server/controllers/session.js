dc071dfa028d7bc3e8d0fd709f37db12d7e8b314
Jack Mocherman <mochermanjack@gmail.com>
2016-10-25 15:17:49 -0500

    added session.js controller for login register functionality

---
+++ server/controllers/session.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+module.exports.register = ({ session, body: { username, password, first_name, last_name} }, res, err) => {
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        res.json({ msg: 'User is already registered' })
+      } else {
+        return new Promise((resolve, reject) => {
+          bcrypt.hash(password, 15, (err, hash) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(hash)
+            }
+          })
+        })
+      }
+    })
+    .then(hash => User.create({ username, password: hash, first_name, last_name }))
+    .then((user) => {
+      session.userId = user.id
+      res.json(user.id)
+    })
+    .catch(err)
+}
+
+// module.exports.login = ({ session, body: { email, password } }, res, err) => {
+//   let loggedInUser;
+//   User.findOne({ email })
+//      .then(user => {
+//        if (user) {
+//         loggedInUser = user;
+//          return new Promise((resolve, reject) =>
+//            bcrypt.compare(password, user.password, (err, matches) => {
+//              if (err) {
+//                reject(err)
+//              } else {
+//                resolve(matches)
+//              }
+//            })
+//          )
+//        } else {
+//          res.send({ msg: 'Email does not exist in our system' })
+//        }
+//      })
+//      .then((matches) => {
+//        if (matches) {
+//          session.email = email
+//          res.json({ loggedInUser, msg: true })
+//        } else {
+//          res.send({ msg: 'Password does not match' })
+//        }
+//       })
+// }
