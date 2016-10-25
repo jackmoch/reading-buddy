08704d8148ed2ae5468864374fb000004f87e590
Jack Mocherman <mochermanjack@gmail.com>
2016-10-24 21:00:32 -0500

    introduced redux promise into middleware

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { Router, browserHistory } from 'react-router'
 import createLogger from 'redux-logger'
+import ReduxPromise from 'redux-promise'
 
 import reducers from './reducers'
 import routes from './routes'
 
 const logger = createLogger()
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore)
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
