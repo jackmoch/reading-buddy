d7ae384c3ce642a951b6b9e51b695c388ecac092
Jack Mocherman <mochermanjack@gmail.com>
2016-10-26 14:24:27 -0500

    added addToWishlist post route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,12 @@ const { Router } = require('express')
 const router = Router()
 
 const session = require('../controllers/session')
+const wishlist = require('../controllers/wishlist')
 
 router.post('/register', session.register)
 
 router.post('/login', session.login)
+
+router.post('/api/addToWishlist', wishlist.addToWishlist)
 
 module.exports = router
