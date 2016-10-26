c2ee18860127d799f181dcaa25859746d423bdd8
Jack Mocherman <mochermanjack@gmail.com>
2016-10-26 11:41:52 -0500

    added ternaries for image and add to wishlist button

--- src/components/book.js
+++ src/components/book.js
@@ -4,6 +4,8 @@ const Book = (book) => {
   return(
     <li>
       {book.volumeInfo.title}
+      { book.volumeInfo.imageLinks ? <img src={book.volumeInfo.imageLinks.thumbnail} /> : null }
+      { book.parentComponent === 'home' ? <button className="btn btn-info btn-block">Add To WishList</button> : null}
     </li>
   )
 }
