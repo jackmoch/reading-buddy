d865636bbdad44c37a06d7d450cb223a7e4ad0a6
Jack Mocherman <mochermanjack@gmail.com>
2016-10-27 10:39:28 -0500

    refactored which buttons appear based on parent container property

--- src/components/book.js
+++ src/components/book.js
@@ -1,12 +1,16 @@
 import React, { PropTypes } from 'react'
 
-const Book = ({ volumeInfo, parentComponent, clickedAddToWishlist, clickedRemoveFromWishlist, id }) => {
+const Book = ({ volumeInfo, parentComponent, clickedAddToWishlist, clickedRemoveFromWishlist, id, clickedAddToCurrentlyReading, clickedRemoveFromCurrentlyReading }) => {
   return(
     <li>
       { volumeInfo.title }
       { volumeInfo.imageLinks ? <img src={volumeInfo.imageLinks.thumbnail} /> : null }
-      { parentComponent === 'home' ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> : null}
       { parentComponent === 'wishlist' ? <button className="btn btn-info btn-block" onClick={ () => clickedRemoveFromWishlist(id)}>Remove From WishList</button> : null}
+      { parentComponent === 'wishlist' ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> : null}
+      { parentComponent === 'home' ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> : null}
+      { parentComponent === 'home'  ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> : null}
+      { parentComponent === 'currently_reading' ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> : null}
+      { parentComponent === 'currently_reading' ? <button className="btn btn-info btn-block" onClick={ () => clickedRemoveFromCurrentlyReading(id)}>Remove From Currently Reading</button> : null}
     </li>
   )
 }
