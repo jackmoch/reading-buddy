386429df1374333ec21262a59939b9bdc3133ba4
Jack Mocherman <mochermanjack@gmail.com>
2016-10-27 09:09:18 -0500

    added remove from wishlist button to book component

--- src/components/book.js
+++ src/components/book.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react'
 
-const Book = ({ volumeInfo, parentComponent, clickedAddToWishlist, id }) => {
+const Book = ({ volumeInfo, parentComponent, clickedAddToWishlist, clickedRemoveFromWishlist, id }) => {
   return(
     <li>
       { volumeInfo.title }
       { volumeInfo.imageLinks ? <img src={volumeInfo.imageLinks.thumbnail} /> : null }
       { parentComponent === 'home' ? <button className="btn btn-info btn-block" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> : null}
+      { parentComponent === 'wishlist' ? <button className="btn btn-info btn-block" onClick={ () => clickedRemoveFromWishlist(id)}>Remove From WishList</button> : null}
     </li>
   )
 }
