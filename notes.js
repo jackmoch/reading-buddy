12f72632796c02fb0420acada9bdf4e014b9b0bd
Jack Mocherman <mochermanjack@gmail.com>
2016-10-27 15:59:07 -0500

    notes from tim

--- notes.js
+++ notes.js
@@ -25,3 +25,45 @@ app.get('/labr/api/getProviders', (req,res) => {
   .catch(console.error)
   res.json({ status: 200})
 })
+
+// Maybe needed for deployment
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
+
+import React from "react";
+import NewRegistration from "./NewRegistration";
+import InvalidRegistration from "./InvalidRegistration";
+
+const Register = React.createClass({
+
+ getInitialState() {
+   return({
+     isRegistered: false, 
+     correctComponent: <Spinner />
+   })
+ },
+
+ componentWillMount() {
+   axios.get('http://localhost:3000/api/' + this.props.params.beaconId)
+     .then(({data: {msg}}) => {
+       if (msg === "Invalid") {
+         this.setState({correctComponent: <InvalidRegistration />})
+       } else {
+         this.setState({correctComponent: <NewRegistration beacon={this.props.params.beaconId} />});
+       }
+     })
+     .catch(console.log);
+ },
+
+ render: function() {
+   return (
+     <div>
+       {this.state.correctComponent}
+     </div>
+   );
+ }
+});
+
+module.exports = Register;
