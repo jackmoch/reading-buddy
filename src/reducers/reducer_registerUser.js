b4102b4fd96de90ab3fee8b710708c424862a76a
Jack Mocherman <mochermanjack@gmail.com>
2016-10-28 12:01:56 -0500

    got rid of unused reducers

--- src/reducers/reducer_registerUser.js
+++
@@ -1,9 +0,0 @@
-import { REGISTER_USER } from '../actions/index'
-
-export default function(state = '', action) {
-	switch (action.type) {
-		case REGISTER_USER:
-			return action.payload.data
-	}
-	return state
-}
