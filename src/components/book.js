e5bb8f77c8bbd56152ed4b1a8672aa010ffcb2aa
Jack Mocherman <mochermanjack@gmail.com>
2016-11-01 11:07:05 -0500

    added new classes to book component

--- src/components/book.js
+++ src/components/book.js
@@ -7,39 +7,39 @@ const Book = ({ volumeInfo, parentComponent, clickedAddToWishlist, clickedRemove
         <div className="thumbnail">
           { volumeInfo.imageLinks ? <img className="group list-group-image" src={volumeInfo.imageLinks.thumbnail} /> : null }
             <div className="caption">
-              <h4 className="group inner list-group-item-heading">{volumeInfo.title}</h4>
-              <p className="group inner list-group-item-text">{volumeInfo.authors[0]}</p>
+              <h4 className="group inner list-group-item-heading bookAuthor">{volumeInfo.title}</h4>
+              <p className="group inner list-group-item-text bookTitle">{volumeInfo.authors[0]}</p>
                 <div className="row">              
                   <div className="col-xs-12 col-md-6">
                     { parentComponent === 'wishlist' ? 
                       <div>
-                        <button className="btn btn-success" onClick={ () => clickedRemoveFromWishlist(id)}>Remove From WishList</button>
-                        <button className="btn btn-success" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> 
-                        <button className="btn btn-success" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedRemoveFromWishlist(id)}>Remove From WishList</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> 
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
                       </div>  
                       : null
                     }
                     { parentComponent === 'home' ? 
                       <div>
-                        <button className="btn btn-success" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> 
-                        <button className="btn btn-success" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button>
-                        <button className="btn btn-success" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button> 
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
                       </div>
                       : null
                     }
                     { parentComponent === 'currently_reading' ? 
                       <div>
-                        <button className="btn btn-success" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> 
-                        <button className="btn btn-success" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
-                        <button className="btn btn-success" onClick={ () => clickedRemoveFromCurrentlyReading(id)}>Remove From Currently Reading</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> 
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCompleted(id)}>Add To Completed</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedRemoveFromCurrentlyReading(id)}>Remove From Currently Reading</button>
                       </div>  
                       : null
                     }
                     { parentComponent === 'completed' ? 
                       <div>
-                        <button className="btn btn-success" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> 
-                        <button className="btn btn-success" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button>
-                        <button className="btn btn-success" onClick={ () => clickedRemoveFromCompleted(id)}>Remove From Completed</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToWishlist(id)}>Add To WishList</button> 
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedAddToCurrentlyReading(id)}>Add To Currently Reading</button>
+                        <button className="btn btn-success bookBtn" onClick={ () => clickedRemoveFromCompleted(id)}>Remove From Completed</button>
                       </div>
                       : null
                     }
